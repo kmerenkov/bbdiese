60603a7c19519adefc79c60febc6a6150e5ed1ea
Konstantin Merenkov <kmerenkov@gmail.com>
2009-02-26 15:27:39 +0300

    Replaced the structure Map with a class Map

--- src/utility.cs
+++ src/utility.cs
@@ -6,16 +6,22 @@ namespace BBDiese
 {
     public static class Utility
     {
-        private struct Map
+        private class Map
         {
-            public string Text;
-            public string Replacement;
+            public string Text {get;set;}
+            public string Replacement {get;set;}
+
+            public Map(string text, string replacement)
+            {
+                this.Text = text;
+                this.Replacement = replacement;
+            }
         }
 
         static private List<Map> html_replacement_map = new List<Map> {
-            new Map { Text = "&", Replacement = "&amp;" },
-            new Map { Text = "<", Replacement = "&lt;" },
-            new Map { Text = ">", Replacement = "&gt;" }
+            new Map("&", "&amp;"),
+            new Map("<", "&lt;"),
+            new Map(">", "&gt;")
         };
 
         public static string EscapeHtml(string text)
